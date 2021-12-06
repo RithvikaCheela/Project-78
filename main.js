4f590276c119a4d2884c0710ac84a2f08d130278
RithvikaCheela <90195216+RithvikaCheela@users.noreply.github.com>
2021-12-06 14:58:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var images = ["https://thumbs.dreamstime.com/b/father-portrait-handsome-man-shirt-jeans-sweater-wearing-glasses-cartoon-character-standing-dad-vector-illustration-86364395.jpg","https://thumbs.dreamstime.com/z/mother-portrait-beautiful-woman-long-brown-hair-young-standing-maternity-mom-vector-illustration-86364538.jpg", "https://png.pngitem.com/pimgs/s/413-4134210_standing-hd-png-download.png" , "https://image.shutterstock.com/image-vector/cute-little-kid-boy-stand-260nw-1872612856.jpg"];
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg",
+"https://thumbs.dreamstime.com/b/father-portrait-handsome-man-shirt-jeans-sweater-wearing-glasses-cartoon-character-standing-dad-vector-illustration-86364395.jpg",
+"https://thumbs.dreamstime.com/z/mother-portrait-beautiful-woman-long-brown-hair-young-standing-maternity-mom-vector-illustration-86364538.jpg",
+ "https://png.pngitem.com/pimgs/s/413-4134210_standing-hd-png-download.png" , 
+"https://image.shutterstock.com/image-vector/cute-little-kid-boy-stand-260nw-1872612856.jpg"];
 var names = ["Fmaily Book","Rupesh Cheela", "Ashvini Cheela", "Rithvika Cheela", "Riyansh Cheela"];
 var i = 0;
 function update()
